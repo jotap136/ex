e3c2b2466b5c549e1acc44a16df23e4310f1c159
jotap136 <joaopcsantos136@gmail.com>
2025-08-12 22:12:03 -0300

    novas atualizações

--- dolar.java
+++ dolar.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
+
 public class dolar {
     
-    
+
+    public static void main(String[] args) {
+        double reais = cash();
+        double trump = converter(reais);
+
+        System.out.println("A conversão de " + reais + " reais para dolar é: " + trump);
+    }
+    public static double cash(){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Informe o valor em reaixx: " );
+        double dinero = leitor.nextDouble();
+        return dinero;
+    }
+
+    public static double converter(double real){
+        Scanner leitor = new Scanner(System.in);
+        double convert = real / 5.40;
+        return convert;
+    }
 }
